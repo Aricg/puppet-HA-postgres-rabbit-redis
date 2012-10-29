2fc921fa01109398f24ceded434d08b029a92f06
aric <actual@aricgardner.com>
2012-10-29 14:58:17 -0400

    split postgres install.pp into install and repo.pp so that the apt-cache is triggered before postgres is installed.

--- postgres/manifests/install.pp
+++ postgres/manifests/install.pp
@@ -10,7 +10,6 @@
 #
 # ==Actions
 # install dependancies for postgres
-# add repo for postgres 9.2
 # install postgres 9.2
 #
 # ==Requires
@@ -20,7 +19,7 @@
 # include postgres::install
 	class postgres::install(
 $package_name = "postgresql-9.2",
-){ Class['postgres::install'] -> Class['postgres::keys'] 
+){ Class ['postgres::repo'] -> Class['postgres::install'] 
 
 
 package { $package_name:                                  
@@ -32,21 +31,6 @@ package { [ ["libpq-dev"],["libpq5"], ["postgresql-contrib-9.2"] ]:
         ensure => installed,
         require => file['/etc/apt/sources.list.d/postgres.list'],
 }
-
-
-exec { "add-pitti-gpg-key":
-        command => "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8683D8A2",
-        unless => "/usr/bin/apt-key list|/bin/grep -c Pitt",
-}
-
-        file {"/etc/apt/sources.list.d/postgres.list":
-                ensure => present,
-                content => "deb http://ppa.launchpad.net/pitti/postgresql/ubuntu precise main                 
-deb-src http://ppa.launchpad.net/pitti/postgresql/ubuntu precise main ",
-                notify => Exec[update-apt-cache],
-                require => Exec["add-pitti-gpg-key"],
-        }
-
 
 
 }
---
+++ postgres/manifests/repo.pp
@@ -0,0 +1,38 @@
+##############################################################
+# @filename : repo.pp
+##############################################################
+#
+# Class: postgres::install
+#
+# This class installs the  postgres 9.2 server's repo
+#
+# Parameters:
+#
+# ==Actions
+# install dependancies for postgres
+# add repo for postgres 9.2
+#
+# ==Requires
+#   This class is dependancy for the other classes 
+#
+# ==Usage
+# include postgres::repo
+	class postgres::repo(
+){
+
+exec { "add-pitti-gpg-key":
+        command => "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8683D8A2",
+        unless => "/usr/bin/apt-key list|/bin/grep -c Pitt",
+}
+
+        file {"/etc/apt/sources.list.d/postgres.list":
+                ensure => present,
+                content => "deb http://ppa.launchpad.net/pitti/postgresql/ubuntu precise main                 
+deb-src http://ppa.launchpad.net/pitti/postgresql/ubuntu precise main ",
+                notify => Exec[update-apt-cache],
+                require => Exec["add-pitti-gpg-key"],
+        }
+
+
+
+}
