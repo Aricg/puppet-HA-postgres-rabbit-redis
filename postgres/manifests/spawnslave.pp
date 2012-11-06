2c60df2f62e4c9a0a00850bbc0725a241b13e399
aric <actual@aricgardner.com>
2012-11-06 12:32:34 -0500

    renamed some variables broken by my last change

--- postgres/manifests/spawnslave.pp
+++ postgres/manifests/spawnslave.pp
@@ -1,17 +1,6 @@
 # this class is not ready for prime time. leving it in as a vestigal organ. 
 # Class: postgres::spawnslave
 class postgres::spawnslave(
-$data_dir = $postgres::config::data_dir,
-$conf_dir = $postgres::config::conf_dir,
-$master_ip = $postgres::config::master_ip,
-$slave_ip = $postgres::config::slave_ip,
-$app_server1 = $postgres::config::app_config1,
-$app_server2 = $postgres::config::app_config2,
-$test_server1 = $postgres::config::test_config1,
-$repluser = $postgres::config::repluser,
-$replpass = $postgres::config::replpass,
-$besuser = $postgres::config::besuser,
-$bespass = $postgres::config::bespass,
 )inherits postgres::config{
 
 #Not going to let puppet do this, puppet shouldn't be expected to be cluster aware. 
